ff20a23a45e8113766bf19927fb2d777912e8459
SagarSRaut <sagar1811raut@gmail.com>
2024-07-16 14:34:53 +0530

    Day12 machine learning

---
+++ Day12/faceplate_assiciations.py
@@ -0,0 +1,28 @@
+import matplotlib.pylab as plt
+import pandas as pd
+from mlxtend.frequent_patterns import apriori, association_rules
+import os
+
+
+fp_df = pd.read_csv(r"C:\Users\Administrator.DAI-PC2\Desktop\jupiter_demo\ML\Datasets-20240429T030248Z-001\Datasets\Faceplate.csv", index_col=0)
+fp_df.head()
+
+
+# create a frequent itemsets
+fp_df = fp_df.astype(bool)
+itemsets = apriori(fp_df, min_support=0.2, use_colnames=True)
+
+
+# and convert into rules
+
+rules = association_rules(itemsets, metric='confidence', min_threshold=0.6)
+rules.info()
+print(rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']])
+
+rules.sort_values(by=['lift'], ascending=False).head(6)
+
+rules_df = rules.sort_values(by=['lift', 'confidence'], ascending=False)
+
+
+relv_rules = rules_df[rules_df['lift']>1]
+relv_rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']]
---
+++ Day12/gaavthiGrocery_association_transaction_gradio.py
@@ -0,0 +1,49 @@
+import gradio as gr 
+import pandas as pd
+import matplotlib.pyplot as plt
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+import httpx
+from mlxtend.preprocessing import TransactionEncoder
+
+
+groceries=[]
+with open(r"C:\Users\Administrator.DAI-PC2\Desktop\jupiter_demo\ML\Datasets-20240429T030248Z-001\Datasets\DataSetA.csv","r")as f:groceries=f.read()
+groceries=groceries.split("\n")
+
+groceries_list=[]
+for i in groceries:
+    groceries_list.append(i.split(","))
+te = TransactionEncoder()
+te_ary=te.fit(groceries_list).transform(groceries_list)
+te_ary
+
+
+fp_df=pd.DataFrame(te_ary[:,1:],columns=te.columns_[1:])
+
+fp_df = fp_df.astype(bool)
+
+def gen_rules(min_sup, min_conf):
+    itemsets = apriori(fp_df, min_support=min_sup, use_colnames=True)
+    rules = association_rules(itemsets, metric='confidence', min_threshold=min_conf)
+    rules = rules[['antecedents', 'consequents', 'support', 'confidence', 'lift']]
+    rules = rules[rules['lift']>1]
+    rules['antecedents'] = [list(x) for x in rules['antecedents'].values]
+    rules['consequents'] = [list(x) for x in rules['consequents'].values]
+    return rules.sort_values(by='lift', ascending=False)
+
+
+demo = gr.Interface(gen_rules, 
+                    inputs= [gr.Slider(value=0.01, step=0.01,
+                                    label="Minimum Support",
+                                    minimum=0.0001, maximum=1),
+                              gr.Slider(value=0.01, step=0.01,
+                                    label="Minimum Confidence",    
+                                    minimum=0.0001, maximum=1)], 
+                    outputs='dataframe')
+
+if __name__ == "__main__":
+    demo.launch()
+    
+    
+    
